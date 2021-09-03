0940a8d308c3fa02d05cd4c3148c0800ba60badf
themainca3000 <77639653+themainca3000@users.noreply.github.com>
2021-09-03 20:27:44 -0300

    Comando reproductor de audio
    En este commit se implemento el comando /pn que reproduce en un canal de voz el audio que necesitemos.

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Require the necessary discord.js classes
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: 32767 });
 
 client.commands = new Collection();
 // Obtenemos unicamente los archivos que terminan con la extension .js
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // Obtenemos una arreglo con el nombre de todos los archivos .js de la carpeta /events
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
 // Definimos los manejadores para cada uno de los eventos obtenidos
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 // Login to Discord with your client's token
